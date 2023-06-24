92ec2b285415d91364fd80b5fe59c721f7c147d0
Federico Sapuppo <fedesapuppo@hotmail.com>
2023-06-24 17:03:48 +0300

    change links for forms

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,10 +1,21 @@
 class RelationshipsController < ApplicationController
     def create
-        other_user = User.find(params[:user_id])
-        @rel = Relationship.new(follower_id: current_user.id,
-                                followed_id: other_user.id
-                                )
-    @rel.save
-    redirect_to user_path(other_user)
+      other_user = User.find(params[:user_id])
+      @rel = Relationship.new(follower_id: current_user.id, followed_id: other_user.id)
+  
+      if @rel.save
+        redirect_to user_path(other_user), notice: 'You are now following this user.'
+      else
+        redirect_to user_path(other_user), alert: 'Failed to follow this user.'
+      end
     end
-end
+  
+    def destroy
+      @rel = Relationship.find(params[:id])
+      other_user = @rel.followed
+      @rel.destroy
+  
+      redirect_to user_path(other_user), notice: 'You have unfollowed this user.'
+    end
+  end
+  
